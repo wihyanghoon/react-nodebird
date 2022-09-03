5672d13eb189474d61213c409921b9b35627af37
wihyanghoon <gidgsn1995@gmail.com>
2022-09-04 04:46:09 +0900

    리듀서 수정

--- prepare/front/reducers/user.js
+++ prepare/front/reducers/user.js
@@ -5,29 +5,87 @@ export const initialState = {
     loginData: {},
 }
 
-export const loginAction = (data) => {
-    console.log(data)
+//로그인 액션
+
+export const loginRequestAction = (data) => {
     return {
-        type: 'LOG_IN',
+        type: 'LOG_IN_REQUSET',
         data,
     }
 }
 
-export const logOutAction = () => {
+export const loginSuccessAction = (data) => {
     return {
-        type: 'LOG_OUT',
+        type: 'LOG_IN_SUCCESS',
+        data,
+    }
+}
+
+export const loginFailureAction = (data) => {
+    return {
+        type: 'LOG_IN_FAILURE',
+        data,
+    }
+}
+
+//로그아웃 액션
+
+export const logoutRequestAction = (data) => {
+    return {
+        type: 'LOG_OUT_REQUSET',
+        data,
+    }
+}
+
+export const logoutSuccessAction = (data) => {
+    return {
+        type: 'LOG_OUT_SUCCESS',
+        data,
+    }
+}
+
+export const logoutFailureAction = (data) => {
+    console.log(data)
+    return {
+        type: 'LOG_OUT_FAILURE',
+        data,
     }
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case 'LOG_IN':
+        case 'LOG_IN_REQUSET':
             return {
                 ...state,
+                isLoggingIn: true,
+                me: action.data
+            }
+        case 'LOG_IN_SUCCESS':
+            return {
+                ...state,
+                isLoggingIn: false,
                 isLoggedIn: true,
                 me: action.data
             }
-        case 'LOG_OUT':
+        case 'LOG_IN_FAILURE':
+            return {
+                ...state,
+                isLoggingIn: false,
+                isLoggedIn: false,
+            }
+        case 'LOG_OUT_REQUSET':
+            return {
+                ...state,
+                isLoggedIn: false,
+                me: null,
+            }
+        case 'LOG_OUT_SUCCESS':
+            return {
+                ...state,
+                isLoggedIn: false,
+                me: null,
+            }
+        case 'LOG_OUT_FAILURE':
             return {
                 ...state,
                 isLoggedIn: false,
