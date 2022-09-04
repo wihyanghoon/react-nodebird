7b7be643d55adb5aa5deb6cd02c2e1f7af39c463
wihyanghoon <gidgsn1995@gmail.com>
2022-09-04 23:42:53 +0900

    configureStore 정리

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -8,11 +8,14 @@ import createSagaMiddleware from 'redux-saga'
 const configureStore = () => {
     const sagaMiddleware = createSagaMiddleware();
     const middlewares = [sagaMiddleware];
+
     const enhancer = process.env.NODE_ENV === 'production'
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
+
     const store = createStore(reducer, enhancer);
     store.sagaTask = sagaMiddleware.run(rootSaga)
+    
     return store;
 }
 
