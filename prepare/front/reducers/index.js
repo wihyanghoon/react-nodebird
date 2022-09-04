9ad8befa3b5ccf2e621de5d48e46b9c58edd22fb
wihyanghoon <gidgns1995@gmail.com>
2022-09-04 23:53:49 +0900

    reducer 함수 정리

--- prepare/front/reducers/index.js
+++ prepare/front/reducers/index.js
@@ -5,6 +5,7 @@ import post from './post'
 
 // ( 이전상태, 액션 ) => 다음상태
 const rootReducer = combineReducers({
+    
     index: (state = {}, action) => {
         switch (action.type) {
             case HYDRATE:
